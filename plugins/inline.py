507c057c87bcfd2e5e17f1d42354d9646e0ca89a
jubo0 <30005726+jubo0@users.noreply.github.com>
2022-10-28 05:53:29 +0600

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,8 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('RED ZONE ™', url='https://t.me/+cCuJ2U2qY-g1MDRl'),
+        InlineKeyboardButton('𝐑𝐄𝐃 𝐙𝐨n𝐞 Pro', url='https://t.me/+JP2ko9e89PtlMGFl'),
     ]
     ]
 def get_cmd(dur):
@@ -47,7 +47,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this[𝐑𝐄𝐃 𝐙𝐨n𝐞 Pro](https://t.me/+JP2ko9e89PtlMGFl) below.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
